5d7a1ca3b3164065234a38824020d578af1c2e56
iuppiter-iovis <verganigiovanni15@gmail.com>
2025-05-23 19:54:41 +0200

    Create puzzles.js

---
+++ puzzles.js
@@ -0,0 +1,11 @@
+const dailyPuzzles = {
+  "2025-05-23": {
+    groups: [
+      { category: "Animali", words: ["cane", "gatto", "criceto", "pesce"] },
+      { category: "Colori", words: ["rosso", "verde", "giallo", "blu"] },
+      { category: "Cibi italiani", words: ["pizza", "pasta", "lasagna", "raviolo"] },
+      { category: "Capitali", words: ["roma", "parigi", "madrid", "berlino"] }
+    ]
+  },
+  // Altri puzzle...
+};
